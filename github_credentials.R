35ac091b577457ccf0ebcdf8db1149435ba07663
Neal Yu <nealxun@gmail.com>
2021-03-10 15:39:41 -0800

    use PAT

---
+++ github_credentials.R
@@ -0,0 +1,10 @@
+# objective: manage git credentials through PAT
+
+install.packages("usethis")
+install.packages("gitcreds")
+
+library(usethis)
+library(gitcreds)
+
+gitcreds::gitcreds_set()
+gitcreds::gitcreds_get()
